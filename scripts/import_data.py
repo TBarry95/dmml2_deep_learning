1d4e1d84ffc53f39a6b76b766e67d3c7c909e9fe
Tiernan Barry <x19141840@student.ncirl.ie>
2020-07-05 16:15:27 +0100

    read in datasets

--- scripts/import_data.py
+++ scripts/import_data.py
@@ -1,4 +1,4 @@
-# DES:
+# DES: Read in pictures from directory
 # BY: Tiernan Barry
 
 import pandas as pd
@@ -17,17 +17,63 @@ import os
 import glob
 
 #########################################################
+# Set RELATIVE working directory (do not use absolute paths!)
+# Working directory should be '.\scripts'
+#########################################################
+
+os.chdir(r".\scripts")
+print("Checking working directory: ", os.getcwd())
+
+#########################################################
 # Get data:
 #########################################################
 
+#######################################
+# Extract pictures into lists:
+#######################################
 
-img_dir = "" # Enter Directory of all images
-data_path = os.path.join(img_dir,'*g')
-files = glob.glob(data_path)
-data = []
-for f1 in files:
-    img = cv2.imread(f1)
-    data.append(img)
+# Locations of each dataset (test, train for both Normal and Pneumonia)
+path_train_normal = "./input_files/train/NORMAL/*g"
+path_train_pneumonia = "./input_files/train/PNEUMONIA/*g"
+path_test_normal = "./input_files/test/NORMAL/*g"
+path_test_pneumonia = "./input_files/test/PNEUMONIA/*g"
+
+# Get list of all files in each directory:
+all_files_train_norm = glob.glob(path_train_normal)
+all_files_train_pneu = glob.glob(path_train_pneumonia)
+all_files_test_norm = glob.glob(path_test_normal)
+all_files_test_pneu = glob.glob(path_test_pneumonia)
+
+# filter pneumonia files for JUST viral?? balance the dataset
+
+
+# List comprehension to get all files into list variables:
+normal_train_pics = []
+pneu_train_pics = []
+normal_test_pics = []
+pneu_test_pics = []
+
+for pic in all_files_train_norm:
+    img = cv2.imread(pic)
+    normal_train_pics.append(img)
+
+for pic in all_files_train_pneu:
+    img = cv2.imread(pic)
+    pneu_train_pics.append(img)
+
+for pic in all_files_test_norm:
+    img = cv2.imread(pic)
+    normal_test_pics.append(img)
+
+for pic in all_files_test_pneu:
+    img = cv2.imread(pic)
+    pneu_test_pics.append(img)
+
+# Test data:
+print(normal_train_pics[0])
+print(pneu_train_pics[0])
+print(normal_test_pics[0])
+print(pneu_test_pics[0])
 
 
 
