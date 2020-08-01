8f7565fd81e037b3012c0c9c48d0ef93882afb0c
Tiernan Barry <x19141840@student.ncirl.ie>
2020-08-01 12:32:38 +0100

    upd

--- scripts/cnn_1.py
+++ scripts/cnn_1.py
@@ -1,6 +1,17 @@
-import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
+# TensorFlow and tf.keras
+# import tensorflow as tf
+# from tensorflow import keras
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import RMSprop
+from keras.preprocessing import image
+from google.colab import files
 
 
 
